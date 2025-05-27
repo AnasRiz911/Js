cbba23720f025a59f96a0ed2c43ebb9ce5879256
Anas <khananas33799@proton.me>
2025-05-27 11:24:10 +0530

    added tasks

---
+++ 10 HTML CSS JS/10f/script.js
@@ -0,0 +1,26 @@
+    function button1 (){
+        const button = document.querySelector('.js-button');
+      if (!button.classList.contains('is-toggled')) {
+        button.classList.add('is-toggled');
+      } else {
+        button.classList.remove('is-toggled');
+      }
+    }
+
+    function button2 (){
+         const button = document.querySelector('.js-button-2');
+      if (!button.classList.contains('is-toggled')) {
+        button.classList.add('is-toggled');
+      } else {
+        button.classList.remove('is-toggled');
+      }
+    }
+
+    function button3 (){
+        const button = document.querySelector('.js-button-3');
+      if (!button.classList.contains('is-toggled')) {
+        button.classList.add('is-toggled');
+      } else {
+        button.classList.remove('is-toggled');
+      }
+    }

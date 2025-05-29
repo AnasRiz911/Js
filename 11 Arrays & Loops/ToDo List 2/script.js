3e2be12c6023d274e719fd15f8a3a563fff02b65
Anas <khananas33799@proton.me>
2025-05-29 15:53:31 +0530

    edit readme

--- 11 Arrays & Loops/ToDo List 2/script.js
+++ 11 Arrays & Loops/ToDo List 2/script.js
@@ -1,32 +1,25 @@
 const todoList = [];
 
 renderTodoList();
-
-function renderTodoList (){
-
+function renderTodoList (){                
     let todolistHTML = '';
-
     for (let i = 0; i < todoList.length; i++){
         const todo = todoList[i];
         const html = `<p>${todo}</p>`;
         todolistHTML += html;
     }
     console.log(todolistHTML);
-
     document.querySelector('.js-todo-list')
     .innerHTML = todolistHTML;
 }
 
 function addToDo(){
-const inputElement = document.querySelector('.js-name-input');
-const name = inputElement.value;
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    todoList.push(name);
 
-
-todoList.push(name);
-console.log(todoList);
-
-inputElement.value = '';
-
-
-renderTodoList();
+    console.log(todoList);
+    
+    inputElement.value = '';
+    renderTodoList();
 }

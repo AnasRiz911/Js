14b5dc5627b8da8ce6894a39d34cd9a1d480a8d8
Anas <khananas33799@proton.me>
2025-05-30 16:48:29 +0530

    added files

--- 11 Arrays & Loops/ToDo List 2/script.js
+++ 11 Arrays & Loops/ToDo List 2/script.js
@@ -5,12 +5,9 @@ const todoList = [{
   name: 'wash dishes',
   dueDate: '2022-12-22'
 }];
-
 renderTodoList();
-
 function renderTodoList() {
   let todoListHTML = '';
-
   todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
@@ -20,10 +17,8 @@ function renderTodoList() {
     `;
     todoListHTML += html;
   });
-
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
-
   document.querySelectorAll('.js-delete-todo-button')
     .forEach((deleteButton, index) => {
       deleteButton.addEventListener('click', () => {
@@ -32,27 +27,21 @@ function renderTodoList() {
       });
     });
 }
-
 document.querySelector('.js-add-todo-button')
   .addEventListener('click', () => {
     addTodo();
   });
-
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-
   const dateInputElement = document.querySelector('.js-due-date-input');
   const dueDate = dateInputElement.value;
-
   todoList.push({
     //name: name,
     //dueDate: dueDate,
     name,
     dueDate
   });
-
   inputElement.value = '';
-
   renderTodoList();
 }

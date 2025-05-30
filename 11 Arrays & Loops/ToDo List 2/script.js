8a3ccce45b168ba9e22a12f40a9a2a6b85d0e33c
Anas <khananas33799@proton.me>
2025-05-30 15:46:18 +0530

    added files

--- 11 Arrays & Loops/ToDo List 2/script.js
+++ 11 Arrays & Loops/ToDo List 2/script.js
@@ -1,25 +1,58 @@
-const todoList = [];
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
 
 renderTodoList();
-function renderTodoList (){                
-    let todolistHTML = '';
-    for (let i = 0; i < todoList.length; i++){
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
-        todolistHTML += html;
-    }
-    console.log(todolistHTML);
-    document.querySelector('.js-todo-list')
-    .innerHTML = todolistHTML;
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
 
-function addToDo(){
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-    todoList.push(name);
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
 
-    console.log(todoList);
-    
-    inputElement.value = '';
-    renderTodoList();
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    //name: name,
+    //dueDate: dueDate,
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  renderTodoList();
 }
